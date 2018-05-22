33c9d036999d525d28ed10cac0251b526b84717b
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2018-05-22 16:00:29 -0700

    added flatten fn

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -18,23 +18,26 @@ app.get('/', function(req, res){
  });  
 
 app.post('/', function(req, res){
-    console.log(req.body)
+    // console.log(req.body)
+    let line = '';
+    let flatten = function(inputObj) {
+        let allKeys = Object.keys(inputObj);
+        for (var i = 0; i < allKeys.length; i++) {
+            if (allKeys[i] !== 'children' && i=== allKeys.length-1) {
+                line += inputObj[allKeys[i]] + '\n';
+                if (inputObj.children) {
+                    
+                }
+            } else if (allKeys[i] !== 'children') {
+                line += inputObj[allKeys[i]] + ',';
+            }
+        }
+        return line;
+    }
+
     res.send(flatten(req.body));
 });
  
  app.listen(3000);
 
-let flatten = function(inputObj) {
-  let line = '';
-  let allKeys = Obj.keys(inputObj);
 
-  for (var i = 0; i < allKeys.length; i++) {
-      if (allKeys[i] !== 'children' && i=== allKeys.length-1) {
-          line += inputObj[allKeys[i]] + '\n';
-      } else if (allKeys[i] !== 'children') {
-          line += inputObj[allKeys[i]] + ',';
-      }
-  }
-
-
-}
