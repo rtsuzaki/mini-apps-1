8b9fe8d15a696124c928f6bd5a13ddfd30a04b7b
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2018-05-22 14:54:33 -0700

    initial commit of challenge 2

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,19 @@
+let post = function() {
+    $.ajax({
+        type: "POST",
+        url: '/',
+        data: $('textarea').val(),
+        success: function(response) {
+            console.log('Successfully posted: ' + response);
+        },
+        contentType: 'application/json',
+      });
+}
 
 
+
+
+
+
+//Add Event Handlers
+$('button').on('click', post)
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,40 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
 
+// //To parse URL encoded data
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+//To parse json data
+app.use(bodyParser.json())
+
+app.use(express.static('client'));
+app.use('/scripts', express.static(__dirname + '/node_modules/'))
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + './index.html');
+ });  
+
+app.post('/', function(req, res){
+    console.log(req.body)
+    res.send(flatten(req.body));
+});
+ 
+ app.listen(3000);
+
+let flatten = function(inputObj) {
+  let line = '';
+  let allKeys = Obj.keys(inputObj);
+
+  for (var i = 0; i < allKeys.length; i++) {
+      if (allKeys[i] !== 'children' && i=== allKeys.length-1) {
+          line += inputObj[allKeys[i]] + '\n';
+      } else if (allKeys[i] !== 'children') {
+          line += inputObj[allKeys[i]] + ',';
+      }
+  }
+
+
+}
